e619a9a4a476cc8c775145178e5cf927be1eec66
d1solis <d1solis@ucsd.edu>
2025-01-03 12:46:19 -0800

    standalone: true updates for both components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
